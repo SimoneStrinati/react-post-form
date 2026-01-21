6c420229d4443f4bc38d2e6451f937637f0524b2
simonestrinati <simone.strinati@outlook.it>
2026-01-21 20:49:19 +0100

    state dichiarati

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,14 @@ import { useState } from 'react'
 function App() {
   //const [count, setCount] = useState(0)
 
+
+  const [author, setAuthor] = useState("");
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [public, setPublic] = useState("");
+
   return (
-    <>
     
-    </>
   )
     
 }
