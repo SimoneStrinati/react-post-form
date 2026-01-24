1d0dc0048ad1a75a31ef02d5985ad81f594cf4ff
simonestrinati <simone.strinati@outlook.it>
2026-01-24 21:05:35 +0100

    esercizio base completato

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,42 @@ function App() {
 
 
   const [author, setAuthor] = useState("");
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  
 
 
   return (
     <div>
+
+      <ul>
+        <li>Inserisci autore: {author}</li>
+        <li>Inserisci titolo: {title}</li>
+        <li>Inserisci testo: {body}</li>
+      </ul>
+
       <form action="">
         <div>
           <input type="text"
+            placeholder='author'
             value={author}
             onChange={e => setAuthor(e.target.value)} />
         </div>
+        <div>
+          <input type="text"
+            placeholder='title'
+            value={title}
+            onChange={e => setTitle(e.target.value)} />
+        </div>
 
-
-
-
+        <div>
+          <input type="text"
+            placeholder='body'
+            value={body}
+            onChange={e => setBody(e.target.value)} />
+        </div>
       </form>
+
     </div>
 
   )
